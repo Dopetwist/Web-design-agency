7e4858e48a0663b04101bdd4b8651d4fea434608
Jonathan <jonathanafugwobi1@gmail.com>
2025-06-02 20:41:40 +0100

    Add Design Section

--- web-design-agency/src/components/Design.jsx
+++ web-design-agency/src/components/Design.jsx
@@ -1,9 +1,18 @@
 import React from "react";
 
-// function Design() {
-//     return (
-        
-//     )
-// }
+function Design() {
+    return (
+        <section className="design" id="design">
+            <h2>Explore our previous Designs</h2>
+
+            <div className="grid-con">
+                <div className="design1"></div>
+                <div className="design2"></div>
+                <div className="design3"></div>
+                <div className="design4"></div>
+            </div>
+        </section>
+    )
+}
 
 export default Design;
--- web-design-agency/src/components/Nav.jsx
+++ web-design-agency/src/components/Nav.jsx
@@ -4,8 +4,8 @@ function Nav() {
     return (
         <nav className="navbar">
             <a href="#home">Home</a>
-            <a href="#design">Designs</a>
             <a href="#service">Services</a>
+            <a href="#design">Designs</a>
             <a href="#about">About</a>
             <a href="#contact">Contact</a>
         </nav>
