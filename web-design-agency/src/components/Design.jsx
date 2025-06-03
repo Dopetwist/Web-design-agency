40b46b3b64637c7b60e5ca9b0b8fd0d89e09e232
Jonathan <jonathanafugwobi1@gmail.com>
2025-06-03 23:32:01 +0100

    Design grid layout

--- web-design-agency/src/components/Design.jsx
+++ web-design-agency/src/components/Design.jsx
@@ -6,10 +6,24 @@ function Design() {
             <h2>Explore our previous Designs</h2>
 
             <div className="grid-con">
-                <div className="design1"></div>
-                <div className="design2"></div>
-                <div className="design3"></div>
-                <div className="design4"></div>
+                <div className="design1 grid">
+                    <img src="/images/design1.jpeg" alt="" />
+                </div>
+                <div className="design2 grid">
+                    <img src="/images/design2.jpeg" alt="" />
+                </div>
+                <div className="design3 grid">
+                    <img src="/images/design3.jpeg" alt="" />
+                </div>
+                <div className="design4 grid">
+                    <img src="/images/design4.jpeg" alt="" />
+                </div>
+                <div className="design5 grid">
+                    <img src="/images/background-1.jpeg" alt="" />
+                </div>
+                <div className="design6 grid">
+                    <img src="/images/design5.jpeg" alt="" />
+                </div>
             </div>
         </section>
     )
